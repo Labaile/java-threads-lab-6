94c9359c4078950721a8062e0a82d8a5f05d9fa0
Labaile <Labaile1@gmail.com>
2022-07-27 12:18:26 -0400

    added code to create executor and prime number logger

---
+++ src/main/java/FirstExecutor.java
@@ -0,0 +1,24 @@
+import java.util.concurrent.Executor;
+
+class FirstExecutor {
+
+    public static void main(String[] args) {
+        SimpleExecutor executor = new SimpleExecutor();
+        MyTask myTask = new MyTask();
+        executor.execute(myTask);
+    }
+
+    static class SimpleExecutor implements Executor {
+        public void execute(Runnable runnable) {
+            Thread newThread = new Thread(runnable);
+            newThread.start();
+        }
+    }
+
+    static class MyTask implements Runnable {
+        public void run() {
+            System.out.println("MyTask is running");
+        }
+    }
+
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,11 +5,12 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         // create an executor
-        ExecutorService executor; 
+        ExecutorService executor = Executors.newSingleThreadExecutor();
 
         while (scanner.hasNext()) {
             int num = scanner.nextInt();
             // submit tasks to your executor
+
         }
     }
 }
@@ -24,5 +25,19 @@ class PrimeLogger implements Runnable {
     @Override
     public void run() {
         // print num if it is prime
+        //int num = 29;
+        boolean flag = false;
+        for (int i = 2; i <= num / 2; ++i) {
+            // condition for nonprime number
+            if (num % i == 0) {
+                flag = true;
+                break;
+            }
+        }
+
+        if (!flag)
+            System.out.println(num + " is a prime number.");
+        else
+            System.out.println(num + " is not a prime number.");
     }
-}
+    }
